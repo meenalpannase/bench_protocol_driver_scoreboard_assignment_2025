208e281aa1f4e43df49e651e9240ba6e46e5f553
Meenal <114145036+meenalpannase@users.noreply.github.com>
2025-10-09 12:10:54 +0530

    Update ahb_driver.sv

--- uvm/driver/ahb_driver/ahb_driver.sv
+++ uvm/driver/ahb_driver/ahb_driver.sv
@@ -9,23 +9,15 @@ Description: Concept of driver writing in UVM
 //write a ahb driver code . follow the coding guidelines 
 
 class ahb_driver extends uvm_driver#(ahb_tx);
-	`uvm_component_utils(ahb_driver)
-	virtual ahb_intf vif; //AHB Interface handle
+	//factory registeration
+//constructor
 
-	function new(string name, uvm_component parent);
-		super.new(name, parent);
-	endfunction
-
-	function void build_phase(uvm_phase phase);
-		super.build_phase(phase);
-
-		end
-	endfunction
-
+//build phase		
+//connect phase	
 	task run_phase(uvm_phase phase); 
 
 		seq_item_port.get_next_item(req);
-			drive_transfer(req);
+		drive_transfer(req);//write ahb drive rlogic here
 		seq_item_port.item_done();
 	end
 	endtask
