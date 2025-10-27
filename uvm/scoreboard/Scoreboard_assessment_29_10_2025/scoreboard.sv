c91b36f75e9a2d85a4a2b9f25eb7d5ab1a5a5677
Meenal <114145036+meenalpannase@users.noreply.github.com>
2025-10-27 10:24:24 +0530

    Create scoreboard.sv

---
+++ uvm/scoreboard/Scoreboard_assessment_29_10_2025/scoreboard.sv
@@ -0,0 +1,19 @@
+/************************************************************************
+Author: Mirafra Technologies Pvt Limited
+        By Meenal Pannase/Priya Ananthakrishnan
+Filename:	Scoreborad.sv  
+Date:   	29th Oct 2025
+Version:	1.0
+Description: Concept of scoreboard writing in UVM 
+***************************************************************************/
+//write scoreboard check for apb protocol, consider single master and single slave .
+
+class scoreboard extends uvm_scoreboard
+  //factory registration
+  //declaration of ports from monitor/refrennce model
+  //constructor
+  //build phase
+  //connect phase
+  //run phase 
+  //write scoreboard check here with expected and actual data
+endclass
